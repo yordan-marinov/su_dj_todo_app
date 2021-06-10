f138c1609b67e24700de0f308547f41f4c276084
yordan-marinov <jordanmarinov8@gmail.com>
2021-06-10 13:39:07 +0300

    Modified core/admin due to register the Task model

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Task, User
+
 # Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "complete", "created_on")
+    list_display_links = ("title",)
+    
