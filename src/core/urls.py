a674959d1a75e1ffef88a7ba35a318dd03dcd657
yordan-marinov <jordanmarinov8@gmail.com>
2021-06-10 13:41:50 +0300

    Modified CRUD logic tree in core/urls

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import task_list
+from .views import TaskDeleteView, TaskList, TaskDetailView, TaskCreateView, TaskUpdateView
 
 urlpatterns = [
-    path('', task_list),
+    path("", TaskList.as_view(), name="tasks"),
+    path("task/<int:pk>/", TaskDetailView.as_view(), name="task"),
+    path("new/", TaskCreateView.as_view(), name="new"),
+    path("edit/<int:pk>", TaskUpdateView.as_view(), name="edit"),
+    path('delete/<int:pk>', TaskDeleteView.as_view(), name='delete'),
 ]
