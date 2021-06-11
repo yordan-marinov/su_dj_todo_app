f9f10453f5f164bd0f4694bdceb22101db0d43fe
yordan-marinov <jordanmarinov8@gmail.com>
2021-06-11 19:08:47 +0300

    Implemented search bar logic in core/views

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,4 @@
+from django.http import request
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 
@@ -52,8 +53,19 @@ class TaskList(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        # context['tasks'] -> queryset of all instances of Task model
+        # print(f'THIS IS THE CONTEXT -> {context["tasks"]}')
         context["tasks"] = context["tasks"].filter(user=self.request.user)
         context["count"] = context["tasks"].filter(complete=False).count()
+        
+        search_input = self.request.GET.get('search_area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(
+                title__istartswith=search_input
+            )
+            
+        context['search_input'] = search_input
+        
         return context
 
 
