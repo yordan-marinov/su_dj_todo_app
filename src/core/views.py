b90d29f5d37fe9105563afcdb2005a7bb0ebc102
yordan-marinov <jordanmarinov8@gmail.com>
2021-06-11 17:02:34 +0300

    Added views and coments

--- src/core/views.py
+++ src/core/views.py
@@ -1,22 +1,65 @@
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
     UpdateView,
     DeleteView,
+    FormView,
 )
 
 from core.models import Task
 
 
+class CustomLoginView(LoginView):
+    template_name = "core/login.html"
+    fields = "__all__"
+    redirect_authenticated_user = True
+
+    def get_success_url(self) -> str:
+        return reverse_lazy("tasks")
+
+
+class RegisterPage(FormView):
+    template_name = "core/register.html"
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy("tasks")
+    
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('task')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
+
 # Create your views here.
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = "tasks"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tasks"] = context["tasks"].filter(user=self.request.user)
+        context["count"] = context["tasks"].filter(complete=False).count()
+        return context
 
-class TaskDetailView(DetailView):
+
+# LoginRequiredMixin deny access to the view if user is not logde in
+# redirect to login page
+class TaskDetailView(LoginRequiredMixin, DetailView):
     model = Task
     # To change the html name to task.html we are using:
     # template_name = "core.task.html after we rename the html file"
@@ -26,21 +69,34 @@ class TaskDetailView(DetailView):
     context_object_name = "task"
 
 
-class TaskCreateView(CreateView):
+# LoginRequiredMixin deny access to the view if user is not logde in
+# redirect to login page
+class TaskCreateView(LoginRequiredMixin, CreateView):
     model = Task
     # template_name = "TEMPLATE_NAME"
-    fields = "__all__"
+    fields = ["title", "description", "complete"]
+    success_url = reverse_lazy("tasks")
+
+    # It shows Todo's of the user who is loged in
+    def form_valid(self, form):
+        # print(f'This is FORM.INSTANCE.USER -> {form.instance.user}')
+        # print(f'This is SELF.REQUEST.USER -> {self.request.user}')
+        form.instance.user = self.request.user
+        return super(TaskCreateView, self).form_valid(form)
+
+
+# LoginRequiredMixin deny access to the view if user is not logde in
+# redirect to login page
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = Task
+    # template_name = "TEMPLATE_NAME"
+    fields = ["title", "description", "complete"]
     success_url = reverse_lazy("tasks")
 
 
-class TaskUpdateView(UpdateView):
-    model = Task
-    # template_name = "TEMPLATE_NAME"
-    fields = "__all__"
-    success_url = reverse_lazy("tasks")
-
-
-class TaskDeleteView(DeleteView):
+# LoginRequiredMixin deny access to the view if user is not logde in
+# redirect to login page
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     # template_name = "TEMPLATE_NAME"
     success_url = reverse_lazy("tasks")
